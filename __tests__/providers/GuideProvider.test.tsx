19490c8013e4e49f5402961668b63ab6030722a0
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2024-10-28 09:10:22 +0000

    remove updateGradeStatus

--- __tests__/providers/GuideProvider.test.tsx
+++ __tests__/providers/GuideProvider.test.tsx
@@ -1,12 +1,10 @@
-import { renderHook, act } from "@testing-library/react"; // Ensure you have this import
-import { GradesGivenStatus } from "../../app/guides/types";
+import { renderHook } from "@testing-library/react"; // Ensure you have this import
 import { useGuide, GuideProvider } from "../../app/providers/GuideProvider"; // Import the provider
 import {
   clearDatabase,
   closeDatabase,
   connect,
   createDummyExtendedGuides,
-  createDummyFetchedGuideWithControl,
   createDummyUser,
 } from "../__mocks__/mongoHandler";
 
@@ -29,35 +27,5 @@ describe("useGuide", () => {
 
     // The guide should initially match the mock
     expect(result.current.guide._id).toBe(mockGuides[0]._id);
-  });
-
-  test("updateGrades function updates the guide context state", async () => {
-    const user = await createDummyUser();
-    const guide = await createDummyFetchedGuideWithControl(user, {
-      feedbackReceived: 2,
-      feedbackGiven: 2,
-      availableToGrade: 2,
-      gradesGiven: 1,
-    });
-
-    const { result } = renderHook(() => useGuide(), {
-      wrapper: ({ children }) => (
-        <GuideProvider guide={guide}>{children}</GuideProvider>
-      ),
-    });
-
-    const docToGrade = guide.availableToGrade[0];
-    docToGrade.grade = 5;
-
-    // Simulate the updateGrades function
-    act(() => {
-      result.current.updateGradeStatus();
-    });
-
-    // Check if gradesGiven contains the updated document
-    expect(result.current.guide.gradesGivenStatus).toEqual(
-      GradesGivenStatus.GRADES_GIVEN
-    );
-    // Add more assertions based on your expected state changes
   });
 });
--- app/providers/GuideProvider.tsx
+++ app/providers/GuideProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { createContext, useState } from "react";
 import React from "react";
-import { ExtendedGuideInfo, GradesGivenStatus } from "../guides/types";
+import { ExtendedGuideInfo } from "../guides/types";
 
 type GuideContextProps = {
   children: React.ReactNode | null;
@@ -10,33 +10,15 @@ type GuideContextProps = {
 
 const GuideContext = createContext<{
   guide: ExtendedGuideInfo;
-  updateGradeStatus: () => void;
-}>({ guide: {} as ExtendedGuideInfo, updateGradeStatus: () => {} });
+}>({ guide: {} as ExtendedGuideInfo });
 
 export const GuideProvider = ({ children, guide }: GuideContextProps) => {
   const [currentGuide, setCurrentGuide] = useState<ExtendedGuideInfo>(guide);
 
   if (!currentGuide._id) return null;
 
-  const updateGradeStatus = () => {
-    const newGradesGivenStatus =
-      currentGuide.gradesGiven.length >= 1
-        ? GradesGivenStatus.GRADES_GIVEN
-        : GradesGivenStatus.AWAITING_FEEDBACK;
-
-    // may be some issue causes as spreading a Mongoose Document
-    // just turns it into a normal JS Object
-    // but don't believe it does not cause any issues
-    const tmp = {
-      ...currentGuide,
-      gradesGivenStatus: newGradesGivenStatus,
-    };
-
-    setCurrentGuide(tmp);
-  };
-
   return (
-    <GuideContext.Provider value={{ guide: currentGuide, updateGradeStatus }}>
+    <GuideContext.Provider value={{ guide: currentGuide }}>
       {children}
     </GuideContext.Provider>
   );
