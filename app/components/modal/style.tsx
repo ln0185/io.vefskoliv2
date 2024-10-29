21c2f3243ee3874495145bf7f4f5b3194749d8ca
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2024-10-29 10:31:48 +0000

    Revert "adjust modal appearance on mobile"
    This reverts commit 0efee633c652f8f5cdda38fe9cb94d7386d41ee9.

--- app/components/modal/style.tsx
+++ app/components/modal/style.tsx
@@ -1,13 +1,9 @@
 import styled from "styled-components";
 
-const deskTopWidth = "450px";
-const mobileWidth = "240px";
-const breakPoint = "600px";
-
 export const ModalWrapper = styled.div`
   display: flex;
   justify-content: center;
-  align-items: flex-end;
+  align-items: center;
   top: 0;
   left: 0;
   width: 100vw;
@@ -15,17 +11,13 @@ export const ModalWrapper = styled.div`
   background-color: rgba(0, 0, 0, 0.5);
   position: fixed;
   z-index: 10;
-
-  @media (min-width: ${deskTopWidth}) {
-    align-items: center;
-  }
 `;
 
 export const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
   background-color: var(--primary-white);
-  height: 95%;
+  max-width: 90vw;
   width: 100%;
   padding: 32px;
   gap: 32px;
@@ -33,12 +25,7 @@ export const ContentWrapper = styled.div`
   border: 1px solid var(--theme-module3-100);
   z-index: 11;
   overflow-y: auto;
-
-  @media (min-width: ${deskTopWidth}) {
-    height: auto;
-    max-width: 90vw;
-    max-height: 90vh;
-  }
+  max-height: 90vh;
 
   @media (min-width: 768px) {
     padding: 64px;
