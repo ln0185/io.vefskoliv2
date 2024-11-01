35125e66d47614e41d5709e57d7aa60105d6a55a
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2024-11-01 11:35:29 +0000

    update useLocalState

--- app/hooks/useLocalState.ts
+++ app/hooks/useLocalState.ts
@@ -2,17 +2,17 @@
 import { useEffect, useState } from "react";
 
 /**
- * Hook to manage state synchronized with localStorage.
+ * Hook to manage state synchronized with localStorage across multiple tabs.
  *
  * @template T - The type of the value to be stored.
  * @param {string} key - The key under which the value is stored in localStorage.
  * @param {T} [defaultValue] - The default value to use if no value is found in localStorage.
  * @param {(value: T) => string} [serialize] - Optional custom serialization function.
  * @param {(value: string) => T} [deserialize] - Optional custom deserialization function.
- * @returns {[T | null, Dispatch<SetStateAction<T | null>>]} A stateful value and a function to update it.
+ * @returns {[T | null, Dispatch<SetStateAction<T | null>>]} A stateful value, a function to update it and a loading state.
  *
  * @example
- * const [value, setValue] = useLocalState<string>("myKey", "default");
+ * const [value, setValue, loadingValue] = useLocalState<string>("myKey", "default");
  *
  * setValue("newValue");
  */
@@ -21,28 +21,43 @@ export function useLocalState<T>(
   defaultValue?: T,
   serialize: (value: T) => string = JSON.stringify,
   deserialize: (value: string) => T = JSON.parse
-): [T | null, (value: T | null | ((prev: T | null) => T | null)) => void] {
+): [
+  T | null,
+  (value: T | null | ((prev: T | null) => T | null)) => void,
+  boolean
+] {
   const item = localStorage.getItem(key);
-  const [storedValue, setStoredValue] = useState<T | null>(() => {
-    try {
-      if (item) {
-        return deserialize(item);
-      }
-    } catch (error) {
-      console.warn(`Error parsing localStorage key "${key}":`, error);
-      localStorage.removeItem(key); // Clean up invalid data
-    }
+  const [storedValue, setStoredValue] = useState<T | null>(null);
+  const [loading, setLoading] = useState(true);
 
-    if (defaultValue !== undefined) {
-      try {
-        serialize(defaultValue); // Check if default value can be serialized
-        return defaultValue;
-      } catch (error) {
-        throw new Error("Default value cannot be serialized");
+  useEffect(() => {
+    const setInitialValue = () => {
+      const item = localStorage.getItem(key);
+      if (item) {
+        try {
+          setStoredValue(deserialize(item));
+          return;
+        } catch (error) {
+          console.warn(`Error parsing localStorage key "${key}":`, error);
+          localStorage.removeItem(key); // Clean up invalid data
+        }
       }
-    }
-    return null;
-  });
+
+      if (defaultValue !== undefined) {
+        try {
+          serialize(defaultValue); // Check if default value can be serialized
+          setStoredValue(defaultValue);
+          return;
+        } catch (error) {
+          throw new Error("Default value cannot be serialized");
+        }
+      }
+      setStoredValue(null);
+    };
+
+    setInitialValue();
+    setLoading(false);
+  }, []);
 
   const updateValue = (value: T | ((prev: T | null) => T | null) | null) => {
     // calculate new value
@@ -70,6 +85,7 @@ export function useLocalState<T>(
     }
   };
 
+  // Listen for changes in other tabs
   useEffect(() => {
     const handleStorageChange = (event: StorageEvent) => {
       if (event.key === key) {
@@ -90,5 +106,5 @@ export function useLocalState<T>(
     };
   }, []);
 
-  return [storedValue, updateValue];
+  return [storedValue, updateValue, loading];
 }
