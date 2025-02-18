4cc4c582188e742ac5f9496529b1c7b68e4af5ff
ln0185 <helene.delaunayratel@gmail.com>
2025-02-18 13:05:20 +0000

    first commit

--- app/pages/dashboard/page.tsx
+++ app/pages/dashboard/page.tsx
@@ -1,7 +1,26 @@
-export default function DashboardPage() {
+import React from "react";
+import Card from "../../components/cards/cards";
+
+const Dashboard: React.FC = () => {
+  const cardsData = [
+    { title: "Guide 1", description: "Description for Card 1" },
+    { title: "Guide 2", description: "Description for Card 2" },
+    { title: "Guide 3", description: "Description for Card 3" },
+    { title: "Guide 4", description: "Description for Card 4" },
+    { title: "Guide 5", description: "Description for Card 5" },
+    { title: "Guide 6", description: "Description for Card 6" },
+    { title: "Guide 7", description: "Description for Card 7" },
+    { title: "Guide 8", description: "Description for Card 8" },
+    { title: "Group Project", description: "Description for Card 9" },
+  ];
+
   return (
-    <div>
-      <h1 className="text-pink-400">Dashboard</h1>
+    <div className="grid-container">
+      {cardsData.map((card, index) => (
+        <Card key={index} title={card.title} description={card.description} />
+      ))}
     </div>
   );
-}
+};
+
+export default Dashboard;
