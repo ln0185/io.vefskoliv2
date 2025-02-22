dd6677c57f7bee071a469ebaa4a6878c5672bc5d
Rafalzie <rafal19191996@gmail.com>
2025-02-22 20:33:01 +0000

    header
    header + filter section

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,10 +1,51 @@
-import { HeaderContainer } from "./style";
+"use client";
 
-export const Header = () => {
+import Image from "next/image";
+import {
+  HeaderContainer,
+  LeftSection,
+  RightSection,
+  UserInfo,
+  IconButton,
+} from "./style";
+
+export const Header: React.FC = () => {
   return (
     <HeaderContainer>
-      <h1>Header</h1>
-      {/* here you need to add the profile component and the text on the left aswell */}
+      {/* Left Section */}
+      <LeftSection>
+        <h1>Hi, Hulda</h1>
+        <p>Let’s finish your task today!</p>
+      </LeftSection>
+
+      {/* Right Section */}
+      <RightSection>
+        <IconButton>
+          <Image
+            src="/search-icon.png" // our search icon
+            alt="Search"
+            width={20}
+            height={20}
+          />
+        </IconButton>
+        <IconButton>
+          <Image
+            src="/notification-icon.png" // our notification icon
+            alt="Notifications"
+            width={20}
+            height={20}
+          />
+        </IconButton>
+        <UserInfo>
+          <Image
+            src="/profile.jpg" // our prifle pictue from figma here
+            alt="User Profile"
+            width={36}
+            height={36}
+            style={{ borderRadius: "50%" }}
+          />
+        </UserInfo>
+      </RightSection>
     </HeaderContainer>
   );
 };
