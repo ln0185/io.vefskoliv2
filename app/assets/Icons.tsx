0f61052fe840f73326be3624b4add70643551b57
ln0185 <helene.delaunayratel@gmail.com>
2025-02-26 18:50:16 +0000

    StyleFixes

--- app/assets/Icons.tsx
+++ app/assets/Icons.tsx
@@ -444,9 +444,9 @@ export const DarkModeIcon = ({
     <path
       d="M21.5 14.0784C20.3003 14.7189 18.9301 15.0821 17.4751 15.0821C12.7491 15.0821 8.91792 11.2509 8.91792 6.52485C8.91792 5.06986 9.28105 3.69968 9.92163 2.5C5.66765 3.49698 2.5 7.31513 2.5 11.8731C2.5 17.1899 6.8101 21.5 12.1269 21.5C16.6849 21.5 20.503 18.3324 21.5 14.0784Z"
       stroke="currentColor"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
@@ -526,16 +526,16 @@ export const NotificationIcon = ({ color }: { color?: string }) => (
       clip-rule="evenodd"
       d="M17.5008 27.7871V27.5681C17.533 26.9202 17.7406 26.2925 18.1024 25.7496C18.7045 25.0975 19.1167 24.2983 19.2957 23.436C19.2957 22.7695 19.2957 22.0935 19.3539 21.427C19.6547 18.2184 22.8273 16 25.9611 16H26.0387C29.1725 16 32.345 18.2184 32.6555 21.427C32.7137 22.0935 32.6555 22.7695 32.704 23.436C32.8854 24.3003 33.2972 25.1019 33.8974 25.7591C34.2618 26.2972 34.4698 26.9227 34.4989 27.5681V27.7776C34.5206 28.648 34.2208 29.4968 33.6548 30.1674C32.907 30.9515 31.8921 31.4393 30.8024 31.5384C27.607 31.8812 24.383 31.8812 21.1876 31.5384C20.0991 31.435 19.0858 30.9479 18.3352 30.1674C17.778 29.4963 17.4822 28.6526 17.5008 27.7871Z"
       stroke="#8E92BC"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M23.5549 34.8516C24.0542 35.4782 24.7874 35.8838 25.5922 35.9785C26.3971 36.0732 27.2072 35.8493 27.8433 35.3562C28.0389 35.2103 28.2149 35.0408 28.3672 34.8516"
       stroke="#8E92BC"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <circle cx="32" cy="19" r="4" fill={color ?? "#D82823"} />
     <rect x="0.5" y="0.5" width="51" height="51" rx="25.5" stroke="#F5F5F7" />
@@ -885,7 +885,7 @@ export const MarkIcon = () => (
       r="7"
       fill="#007AFF"
       stroke="white"
-      stroke-width="2"
+      strokeWidth="2"
     />
   </svg>
 );
